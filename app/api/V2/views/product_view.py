69794033b114a1b9713fb36636df9bb26dfcb611
kmwangemi <mwangemik@gmail.com>
2018-11-02 02:15:59 +0300

    [Feature] add files for running

--- app/api/V2/views/product_view.py
+++ app/api/V2/views/product_view.py
@@ -14,25 +14,31 @@ def create_product():
     product2 = Product(data['product_name'], data['category'], data['quantity'], data['price'], data['description'])
     response = product2.add_products()
     if response:
-        return jsonify({'message' : 'Product created successfully'}), 201
+        return jsonify({'message' : 'Product already exists'}), 201
     else:
-            return jsonify({'message' : 'Product already exists'})
+            return jsonify({'message' : 'Product created successfully'})
 
 
 @product.route('/products',methods=['GET'])
 def get_all_products():
     """Gets all products"""
-    data = request.get_json()
-    product2 = Product(data['product_name'], data['category'], data['quantity'], data['price'], data['description'])
-    response = product2.add_products()
     response = product2.get_all_products()
     return jsonify({'Products' : response}), 200
 
 @product.route('/products/<productId>', methods=['GET']) 
 def get_one_product(productId):
     """Gets one product"""
-    data = request.get_json()
-    product2 = Product(data['product_name'], data['category'], data['quantity'], data['price'], data['description'])
-    response = product2.add_products()
     response = product2.get_one_product(productId)
     return jsonify({'Product' : response}), 200
+
+@product.route('/products/<productId>', methods=['PUT']) 
+def update_product(productId):
+    """Updates product"""
+    response = product2.update_product(productId)
+    return response
+
+@product.route('/products/<productId>', methods=['DELETE']) 
+def delete_product(productId):
+    """Deletes product"""
+    response = product2.delete_product(productId)
+    return response    
--- app/api/V2/views/user_view.py
+++ app/api/V2/views/user_view.py
@@ -1,55 +1,30 @@
-from flask import request, jsonify, Blueprint
 from werkzeug.security import generate_password_hash, check_password_hash
-from functools import wraps
-import os
-import datetime
-import jwt
+from flask import request, jsonify, Blueprint, make_response
 import re
-
+from psycopg2.extras import RealDictCursor
+from db_con import db_url
+import psycopg2
+conn = psycopg2.connect(db_url)
+cur = conn.cursor(cursor_factory=RealDictCursor)
 from app.api.V2.models.user_model import User
-SECRET_KEY = os.getenv('SECRET_KEY')
 
 user2 = User()
 
 user = Blueprint('user', __name__, url_prefix='/api/v2')
+auth = Blueprint('auth', __name__, url_prefix='/api/v2/auth')
 
 #user routes
-'''
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = None
-        if 'x-access-token' in request.headers:
-            token = request.headers['x-access-token']
-        if not token:
-            return jsonify({'message': 'Token is missing, login to get token'}), 401
-        try:
-            data = jwt.decode(token, SECRET_KEY)
-            current_user = user2.get_one_user[email]
-            if not current_user:
-                return jsonify({"message": "You are not logged in"}), 401   
-        except:
-            return jsonify({'message': 'Token is invalid or Expired!'}), 401
 
-        return f(current_user, *args, **kwargs)
-    return decorated
-'''
-
-@user.route('/register', methods=['POST'])
-def create_user():
-    """Creates a new user"""
-    data = request.get_json()
-    hashed_password = generate_password_hash(data['password'], method='sha256')
-    if not data:
-        return jsonify({'message' : 'Please insert data'})
-    if not data or not data['fname'] or not data['lname'] or not data['email'] or not hashed_password:
-        return jsonify({'message': "email or password missing"}), 400
-
-    if not re.match(r'\A[0-9a-zA-Z!@#$%&*]{6,20}\Z', data['password']):
-        return jsonify({"Message": "Password must be 6-20 Characters"}), 406    
-    user_info = User(data['fname'], data['lname'], data['email'], hashed_password, data['role'])
-    user_info.add_users()
-    return jsonify({'message' : 'User created'}), 201
+@auth.route('/signup', methods=['POST'])
+def signup():
+    response = user2.signup()
+    return response
+        
+@auth.route('/login', methods=['POST'])
+def login():
+    """users will login to the app via this route"""
+    response = user2.login()
+    return response    
    
 @user.route('/users', methods=['GET'])
 def get_all_users():
@@ -61,25 +36,5 @@ def get_all_users():
 def get_one_user(userId):
     """Gets a single user"""
     response = user2.get_one_user(userId)
-    return jsonify({'User' : response}), 200
-
-
-
-@user.route('/login', methods=['POST'])
-def login():
-    """users will login to the app via this route"""
-    auth = request.authorization
-    if not auth or not auth['email'] or not auth['password']:
-        return jsonify({'message' : 'Could not verify'}), 401 
-
-    user = get_one_user(auth['email'])
-    if not user:
-        return jsonify({'message' : 'Could not verify'}), 401
-    
-    if check_password_hash(user['password'], auth['password']):
-        token = jwt.encode({'id' : user[id], 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=240)}, SECRET_KEY)
-            
-        return jsonify({"token": token.decode('UTF-8')})
-
-    return jsonify({'message' : 'Could not verify'}), 401
+    return jsonify({'User' : response}), 200 
 
